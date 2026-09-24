5e4eb9477bc5f64910b84c466611e103595fa76e
AKhilesh Singh <akhileshsxiv@gmail.com>
2026-09-24 14:46:13 +0530

    git init changes

--- vars/djangoDeploy.groovy
+++ vars/djangoDeploy.groovy
@@ -1,6 +1,4 @@
 def call(Map config = [:]) {
-    def workspacePath = config.workspacePath ?: '/home/osianlap07/Projects/Need_Analysis/etli-need-analysis-backend'
-    def rootPath = '/home/osianlap07/Projects/Need_Analysis'
     def credentialsId = config.credentialsId ?: 'django-env-file'
     def repoUrl = config.repoUrl ?: 'https://repo.osian.io/osianinfotech/edelweiss-tokio/etli-need-analysis-backend.git'
     def repoCreds = config.repoCreds ?: 'osian-repo-credentials'
@@ -9,64 +7,61 @@ def call(Map config = [:]) {
     pipeline {
         agent any
 
+        parameters {
+            string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'Enter the branch name or feature branch to build (e.g., feature/login-flow, main, master)')
+        }
+
         stages {
             stage('Checkout Code') {
                 steps {
-                    dir(workspacePath) {
-                        checkout([
-                            $class: 'GitSCM',
-                            branches: [[name: '*/master']],
-                            userRemoteConfigs: [[
-                                url: repoUrl,
-                                credentialsId: repoCreds
-                            ]]
-                        ])
-                    }
+                    // Jenkins checks out natively in its own workspace root. No 'dir()' wrapper needed here!
+                    checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: "${params.BRANCH_NAME}"]],
+                        userRemoteConfigs: [[
+                            url: repoUrl,
+                            credentialsId: repoCreds
+                        ]]
+                    ])
                 }
             }
 
             stage('Setup Environment & Dependencies') {
                 steps {
-                    dir(workspacePath) {
-                        withCredentials([file(credentialsId: credentialsId, variable: 'ENV_FILE_PATH')]) {
-                            sh '''
-                                cp $ENV_FILE_PATH need_analysis_backend/.env
-                                python3 -m venv venv
-                                ./venv/bin/pip install --upgrade pip
-                                ./venv/bin/pip install -r requirements.txt
-                                ./venv/bin/pip install cryptography
-                            '''
-                        }
+                    withCredentials([file(credentialsId: credentialsId, variable: 'ENV_FILE_PATH')]) {
+                        sh '''
+                            cp $ENV_FILE_PATH need_analysis_backend/.env
+                            python3 -m venv venv
+                            ./venv/bin/pip install --upgrade pip
+                            ./venv/bin/pip install -r requirements.txt
+                            ./venv/bin/pip install cryptography
+                        '''
                     }
                 }
             }
 
             stage('OWASP: Dependency Check') {
                 steps {
-                    dir(workspacePath) {
-                        withCredentials([string(credentialsId: 'nvd-api-key-id', variable: 'NVD_API_KEY')]) {
-                            dependencyCheck additionalArguments: "--scan . --disableAssembly --nvdApiKey ${env.NVD_API_KEY}", odcInstallation: 'OWASP'
-                        }
-                        dependencyCheckPublisher pattern: 'dependency-check-report.xml'
+                    withCredentials([string(credentialsId: 'nvd-api-key-id', variable: 'NVD_API_KEY')]) {
+                        dependencyCheck additionalArguments: "--scan . --disableAssembly --nvdApiKey ${env.NVD_API_KEY}", odcInstallation: 'OWASP'
                     }
+                    dependencyCheckPublisher pattern: 'dependency-check-report.xml'
                 }
             }
 
             stage('SonarQube: Code Analysis') {
                 steps {
-                    dir(workspacePath) {
-                        withSonarQubeEnv("${sonarServer}") {
-                            script {
-                                def scannerHome = tool 'SonarQube'
-                                
-                                sh """
-                                    ${scannerHome}/bin/sonar-scanner \
-                                      -Dsonar.projectKey=etli-need-analysis-backend \
-                                      -Dsonar.projectName=etli-need-analysis-backend \
-                                      -Dsonar.sources=. \
-                                      -Dsonar.exclusions=**/venv/**,**/staticfiles/**,**/media/**,**/certificate_pdf/templates/**,**/migrations/**
-                                """
-                            }
+                    withSonarQubeEnv("${sonarServer}") {
+                        script {
+                            def scannerHome = tool 'SonarQube'
+                            
+                            sh """
+                                ${scannerHome}/bin/sonar-scanner \
+                                  -Dsonar.projectKey=etli-need-analysis-backend \
+                                  -Dsonar.projectName=etli-need-analysis-backend \
+                                  -Dsonar.sources=. \
+                                  -Dsonar.exclusions=**/venv/**,**/staticfiles/**,**/media/**,**/certificate_pdf/templates/**,**/migrations/**
+                            """
                         }
                     }
                 }
@@ -89,21 +84,17 @@ def call(Map config = [:]) {
 
             stage('Run Migrations') {
                 steps {
-                    dir(workspacePath) {
-                        sh '''
-                            ./venv/bin/python manage.py migrate --noinput
-                        '''
-                    }
+                    sh '''
+                        ./venv/bin/python manage.py migrate --noinput
+                    '''
                 }
             }
 
             stage('Restart Docker Service') {
                 steps {
-                    dir(workspacePath) { 
-                        sh '''
-                            docker compose -f ../docker-compose.yml restart || docker-compose -f ../docker-compose.yml restart
-                        '''
-                    }
+                    sh '''
+                        docker compose restart || docker-compose restart
+                    '''
                 }
             }
         }
