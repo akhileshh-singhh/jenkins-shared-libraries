66cfbad8c44cfbc3847e76cb3f1a04e3b793d520
AKhilesh Singh <akhileshsxiv@gmail.com>
2026-09-24 14:51:19 +0530

    git init changes

--- vars/djangoDeploy.groovy
+++ vars/djangoDeploy.groovy
@@ -1,5 +1,4 @@
 def call(Map config = [:]) {
-    def subfolderName = config.subfolderName ?: '.' // Adjust if your repo root contains a subfolder
     def credentialsId = config.credentialsId ?: 'django-env-file'
     def repoUrl = config.repoUrl ?: 'https://repo.osian.io/osianinfotech/edelweiss-tokio/etli-need-analysis-backend.git'
     def repoCreds = config.repoCreds ?: 'osian-repo-credentials'
@@ -15,7 +14,6 @@ def call(Map config = [:]) {
         stages {
             stage('Checkout Code') {
                 steps {
-                    // Jenkins checks out natively in its workspace root cleanly
                     checkout([
                         $class: 'GitSCM',
                         branches: [[name: "${params.BRANCH_NAME}"]],
@@ -29,18 +27,15 @@ def call(Map config = [:]) {
 
             stage('Setup Environment & Dependencies') {
                 steps {
-                    script {
-                        // If your project files are in the root of the workspace, leave subfolderName as '.'
-                        // If they are nested, specify the folder name here
-                        withCredentials([file(credentialsId: credentialsId, variable: 'ENV_FILE_PATH')]) {
-                            sh '''
-                                cp $ENV_FILE_PATH need_analysis_backend/.env
-                                python3 -m venv venv
-                                ./venv/bin/pip install --upgrade pip
-                                ./venv/bin/pip install -r requirements.txt
-                                ./venv/bin/pip install cryptography
-                            '''
-                        }
+                    withCredentials([file(credentialsId: credentialsId, variable: 'ENV_FILE_PATH')]) {
+                        sh '''
+                            mkdir -p need_analysis_backend
+                            cp $ENV_FILE_PATH need_analysis_backend/.env
+                            python3 -m venv venv
+                            ./venv/bin/pip install --upgrade pip
+                            ./venv/bin/pip install -r requirements.txt
+                            ./venv/bin/pip install cryptography
+                        '''
                     }
                 }
             }
